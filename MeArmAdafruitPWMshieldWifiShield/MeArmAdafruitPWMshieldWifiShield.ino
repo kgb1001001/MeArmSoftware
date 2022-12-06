64ff7c7ddafa56223b3fa4a4ea52b345c72d7fd7
Kyle Brown <kgb1001001@aol.com>
2022-12-06 16:16:37 -0500

    Update MeArmAdafruitPWMshieldWifiShield.ino
    First working version ready for others

--- MeArmAdafruitPWMshieldWifiShield/MeArmAdafruitPWMshieldWifiShield.ino
+++ MeArmAdafruitPWMshieldWifiShield/MeArmAdafruitPWMshieldWifiShield.ino
@@ -42,8 +42,8 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // our servo # counter
 int firsttime = 0;
 
-char ssid[] = "Kyles iPhone"; //  your network SSID (name)
-char pass[] = "m0nk3ybreath";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = ""; //  your network SSID (name)
+char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
